1b59f6cc5711dae1fa27dbca336ca703e8c0f91a
Andres Jose Beltran Ortiz <batsu_75w@hotmail.com>
2025-07-26 09:57:44 -0500

    header, create https, product class

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,29 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./layout/header/header";
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
+  base_url = 'https://localhost:5001/api/'
+  private http = inject(HttpClient)
   protected readonly title = signal('client');
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(this.base_url + 'products').subscribe({
+      next: response => this.products = response.data,
+      error: error => console.log(error),
+      complete: () => console.log('complete')
+    });
+  }
+
+  //constructor(private http: HttpClient) {}
 }
